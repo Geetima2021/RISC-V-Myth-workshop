2aa5eea287ed6d4eeb7825cb954d27cffda378e0
Geetima Kachari <er.geetimakachari@gmail.com>
2021-06-23 14:16:39 +0530

    Codes

---
+++ Codes/sum1to9.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+
+int main () {
+	int i,sum = 0, n = 9;
+	for (i = 1; i <=n; ++i) {
+		sum += i;
+	}
+	printf("The sum of the number from 1 to %d is %d\n", n,sum);
+	return 0;
+	}
